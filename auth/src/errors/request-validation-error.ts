6a8bab4a07a2c271b8ef406b7f7a89eb31b92e30
sebastiankut <sebastian_kut@yahoo.co.uk>
2022-06-29 08:44:00 +0200

    added error handling

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,19 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestVaidationError extends Error {
+export class RequestVaidationError extends CustomError {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Validation Error');
 
-    // Because extending built-in class:
+    // Because extending built-in class we use following:
     Object.setPrototypeOf(this, RequestVaidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => {
+      return { message: err.msg, field: err.param };
+    });
   }
 }
--- auth/src/middlewares/error-handeler.ts
+++ auth/src/middlewares/error-handeler.ts
@@ -1,7 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestVaidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-
+import { CustomError } from '../errors/custom-error';
 // To be able to add this middleware as an error handerler to express and in order to express recognize it as error
 //  handeler (so then we can use it like "throw new Error") it needs to have 4 arguments like below
 export const errorHandeler = (
@@ -10,15 +8,11 @@ export const errorHandeler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestVaidationError) {
-    console.log('This is a validation error');
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    console.log('This is database connection error');
-  }
-
-  res.status(400).send({
-    message: err.message,
-  });
+  return res
+    .status(400)
+    .send({ errors: [{ message: 'Something went wrong' }] });
 };
