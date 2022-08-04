2a38bec98d6e4589c9bc0ad4a812b2d41fb8075e
sebastiankut <sebastian_kut@yahoo.co.uk>
2022-08-04 16:19:30 +1000

    added payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  currentUser,
+  errorHandeler,
+  NotFoundError,
+} from '@idigitalbeatzgittix/common';
+
+const app = express();
+// tell express to trust nginx proxy that we use for https connections
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // signed means encrypted cookie data, we dnt want that as its hard to decrypt data between different ;anguages incase we build other microservices using different languages, and JWT that will be used inside the cookie is safe already and encrypted
+    signed: false,
+    // cookie is only to be sent over HTTPS unless testing environment
+    secure: process.env.NODE_ENV !== 'test',
+
+    // to test app through http we can uncomment
+    // secure: false,
+  })
+);
+
+// check every incoming request if user is logged in, always after use(cookieSession) as it checks cookie for jwt
+// then every route we want user to be authenticated we just add requireAuth middleware to that specific route
+app.use(currentUser);
+
+app.all('/*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandeler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,68 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { generateMongoId } from './util/generate-id';
+
+// declaration of the global.signin that we are adding to use in tests
+declare global {
+  var signin: () => string[];
+}
+
+// instead of creating real instance of nats client in the test we will create a fake instance
+// by putting file with the same name as the one we are trying to fake in __mocks__ directory
+// then giving realtive path to the real file to jest.mock()
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+// Specify what to do in beforeAll hook - runs before all of our tests
+
+beforeAll(async () => {
+  // we need to specify JWT secret key because this only exists inside of our conteiner and not in the test environment
+  process.env.JWT_KEY = 'asdfhh';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+// beforeEach runs before each of our tests
+// Delete all collections from our in memory instance of mongoDb
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// after all test close in memory mongo connection
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+// Global signin property (function), for convinience so we dnt have to export it all the time
+// this will only be available in the test environment not in our express app and used inside tests
+// to sign up and return cookie
+
+global.signin = () => {
+  // We dnt want to make a request to auth service to log in as tickets service should be self contained
+  // so we will fake a cookie for tests - this is how auth middleware check if we are authenticated
+  // Build JWT payload {id, email}
+  const payload = {
+    id: generateMongoId(),
+    email: 'test@test.com',
+  };
+  // Create JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  // Build session object {jwt: MY_JWT}
+  const session = { jwt: token };
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  // Take JSON and encode it as BASE64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  // Return the string that looks like cookie - supertest requires cookies to be an array
+  return [`session=${base64}`];
+};
